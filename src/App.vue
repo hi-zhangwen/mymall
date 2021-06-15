649c8d2b911a73292a0b7fc695d0f67b648d8c2a
like <namezvv@qq.com>
2021-06-15 20:17:59 +0800

    添加配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+  </div>
 </template>
 
 <script>
@@ -11,4 +12,5 @@ export default {
 </script>
 
 <style>
+@import 'assets/css/base.css'
 </style>
